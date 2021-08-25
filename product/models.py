6e87c0f8357ae56d9320bc4747f9d52402fc54fe
Salahuddin Shaikh <33448178+salahoddin88@users.noreply.github.com>
2021-08-25 14:54:14 +0530

    Initial commit

--- product/models.py
+++ product/models.py
@@ -3,14 +3,21 @@ from django.db.models.base import Model
 
 
 class ProductCategory(models.Model):
+    """ Product Category Model Class """
     name = models.CharField(max_length=255)
     status = models.BooleanField(default=True)
     
+    """ 
+        Return a object representation string. Model Object PK to Category Title. 
+        example ProductCategory Object (1) to Shoes/Jeans/Tshirt 
+    """
     def __str__(self):
         return str(self.name)
 
 
 class Product(models.Model):
+    """ Product Model Class """
+    # ProductCategory PK Refrence as FK because ProductCategory can have multiple products
     product_category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
@@ -18,6 +25,10 @@ class Product(models.Model):
     image = models.ImageField()
     status = models.BooleanField(default=True)
     
+    """ 
+        Return a object representation string. Model Object PK to Product. 
+        example Product Object (1) to Cotton Jeans. 
+    """ 
     def __str__(self):
         return str(self.name)
     
